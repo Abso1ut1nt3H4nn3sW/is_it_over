8ec9afec3ce40c5c27ba827a8a35969993923fe5
hanwet <hanneswettergren@gmail.com>
2024-12-18 20:47:11 +0100

    minor changes

--- evo.py
+++ evo.py
@@ -3,7 +3,7 @@ from time import strftime, localtime, sleep
 
 def get_new_price(stock):
     """
-    Gets current stock price
+    Get current stock price
     """
     ticker = yf.Ticker(stock)
     current_price = ticker.fast_info['lastPrice']
@@ -13,21 +13,22 @@ def main():
     """
     Check if it is over or if we won, every ten seconds.
     """
-    evo_old = get_new_price("EVO.ST")
-    price_history = {evo_old}
+    evo_low = get_new_price("EVO.ST")
+    evo_high = evo_low
     current_time = strftime('%H:%M:%S', localtime())
     print(f"[{current_time}] Tjena Jojje! EVO just nu på: {evo_old}\n")
     while True:
         evo_new = get_new_price("EVO.ST")
-        if evo_new < evo_old:
+        if evo_new < evo_low:
             current_time = strftime('%H:%M:%S', localtime())
             print(f"[{current_time}] Jojje bros... It's over")
             print(f"EVO just nu på: {evo_new}\n")
             evo_old = evo_new
-        elif evo_new > max(price_history):
+        elif evo_new > evo_high:
+            current_time = strftime('%H:%M:%S', localtime())
             print(f"[{current_time}] Jojje bros... We won!")
             print(f"EVO just nu på: {evo_new}\n")
-            price_history.add(evo_new)
+            evo_high = evo_new
         sleep(10)
 
 if __name__ == "__main__":
