f02a17ae322506bf8b7fda621d5e2a0540c661f2
hanwet <hanneswettergren@gmail.com>
2024-12-18 15:59:04 +0100

    Changed the output string and added function to check if we won

--- evo.py
+++ evo.py
@@ -1,11 +1,34 @@
 import yfinance as yf
-import time
+from time import strftime, localtime, sleep
 
-evo_old = yf.Ticker("EVO.ST")
-print(f"[{time.strftime('%H:%M:%S', time.localtime())}] Tjena Jojje! EVO ligger just nu på {round(evo_old.fast_info['lastPrice'], 2)}")
-while True:
-    evo_new = yf.Ticker("EVO.ST")
-    if round(evo_new.fast_info['lastPrice'], 2) < round(evo_old.fast_info['lastPrice'], 2):
-        print(f"[{time.strftime('%H:%M:%S', time.localtime())}] Tjena Jojje! EVO har nu gått ner till {round(evo_new.fast_info['lastPrice'], 2)}")
-        evo_old = evo_new
-    time.sleep(10)
+def get_new_price(stock):
+    """
+    Gets current stock price
+    """
+    ticker = yf.Ticker(stock)
+    current_price = ticker.fast_info['lastPrice']
+    return round(current_price, 2)
+
+def main():
+    """
+    Check if it is over or if we won, every ten seconds.
+    """
+    evo_old = get_new_price("EVO.ST")
+    price_history = {evo_old}
+    current_time = strftime('%H:%M:%S', localtime())
+    print(f"[{current_time}] Tjena Jojje! EVO just nu på: {evo_old}\n")
+    while True:
+        evo_new = get_new_price("EVO.ST")
+        if evo_new < evo_old:
+            current_time = strftime('%H:%M:%S', localtime())
+            print(f"[{current_time}] Jojje bros... It's over")
+            print(f"EVO just nu på: {evo_new}\n")
+            evo_old = evo_new
+        elif evo_new > max(price_history):
+            print(f"[{current_time}] Jojje bros... We won!")
+            print(f"EVO just nu på: {evo_new}\n")
+            price_history.add(evo_new)
+        sleep(10)
+
+if __name__ == "__main__":
+    main()
